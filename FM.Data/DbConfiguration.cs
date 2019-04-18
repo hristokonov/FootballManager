f248cfc25d021ec772d2d9b87f03cbb390ae4643
hristokonov <hristo_konov@abv.bg>
2019-04-18 11:07:06 +0300

    FinalVersion

--- FM.Data/DbConfiguration.cs
+++ FM.Data/DbConfiguration.cs
@@ -2,7 +2,6 @@
 {
     public static class DbConfiguration
     {
-        public const string ConnectionString = @"Server=localhost\SQLEXPRESS;Database=FM.V6;Trusted_Connection=True;"; //Hristo
-        //public const string ConnectionString = @"Server=DESKTOP-K77C1A5\SQLEXPRESS;Database=Fm-V5;Trusted_Connection=true;"; //Petar
+        //not here 
     }
 }
